f68099160a21a5751994f1728c674c6765cad836
iven <iven@coins.ph>
2023-01-10 18:44:53 +0800

    update

--- app/src/main/java/com/go23wallet/MyApp.kt
+++ app/src/main/java/com/go23wallet/MyApp.kt
@@ -7,7 +7,7 @@ class MyApp : Application() {
 
     override fun onCreate() {
         super.onCreate()
-        Go23WalletManage.getInstance()
+        Go23WalletManage.getInstance().setDebug(true)
             .build(applicationContext, "j9ASxn5REHG8akytevRYZwCp", "QHXFT28Nu1u4R7IiGBlFCVXF")
     }
 }
