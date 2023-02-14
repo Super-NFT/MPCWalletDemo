e4b606e1364affa59117ea78f2d37023a4b1801d
sean <sean.wu@coins.ph>
2023-02-14 17:05:57 +0800

    fix ui

--- app/src/main/java/com/go23wallet/MyApp.kt
+++ app/src/main/java/com/go23wallet/MyApp.kt
@@ -9,7 +9,7 @@ class MyApp : Application() {
         super.onCreate()
 //        Go23WalletManage.getInstance().setDebug(false)
 //            .build(applicationContext, "OcHB6Ix8bIWiOyE35ze6Ra9e", "KX6OquHkkKQmzLSncmnmNt2q")
-        Go23WalletManage.getInstance().setDebugModel(true)
+        Go23WalletManage.getInstance().setDebugModel(true).setUseStyleFont(true)
             .build(applicationContext, "j9ASxn5REHG8akytevRYZwCp", "QHXFT28Nu1u4R7IiGBlFCVXF")
     }
 }
