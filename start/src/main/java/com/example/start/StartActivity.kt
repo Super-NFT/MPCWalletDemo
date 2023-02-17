6b4716968e2d1ba77e450f620635373c3feec618
sean <sean.wu@coins.ph>
2023-02-17 18:36:36 +0800

    set demo

--- start/src/main/java/com/example/start/StartActivity.kt
+++ start/src/main/java/com/example/start/StartActivity.kt
@@ -3,24 +3,32 @@ package com.example.start
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.TextView
+import androidx.appcompat.widget.AppCompatEditText
+import androidx.appcompat.widget.AppCompatTextView
 import com.go23wallet.mpcwalletdemo.data.PhoneInfo
 import com.go23wallet.mpcwalletdemo.data.UserInfo
 import com.go23wallet.mpcwalletdemo.wallet.WalletActivity
 
 class StartActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_start)
-        findViewById<TextView>(R.id.btn).setOnClickListener {
+        val etUniqueId = findViewById<AppCompatEditText>(R.id.et_unique_id)
+        val etNickname = findViewById<AppCompatEditText>(R.id.et_nickname)
+        val etAvatar = findViewById<AppCompatEditText>(R.id.et_avatar)
+        val etEmail = findViewById<AppCompatEditText>(R.id.et_email)
+        val etDial = findViewById<AppCompatEditText>(R.id.et_dial)
+        val etPhone = findViewById<AppCompatEditText>(R.id.et_phone)
+        findViewById<AppCompatTextView>(R.id.tv_confirm).setOnClickListener {
             startActivity(Intent(this, WalletActivity::class.java).apply {
                 putExtra(
                     "user_info", UserInfo(
-                        "qqqa@coins.ph",
-                        "qqqa@coins.ph",
-                        PhoneInfo("+86", "13671697524"),
-                        "test123",
-                        "https://d2vvute2v3y7pn.cloudfront.net/logo/Avalanche/0xe0bb6feD446A2dbb27F84D3C27C4ED8EA7603366.webp"
+                        etUniqueId.text.toString(),
+                        etEmail.text.toString(),
+                        PhoneInfo(etDial.text.toString(), etPhone.text.toString()),
+                        etNickname.text.toString(),
+                        etAvatar.text.toString(),
                     )
                 )
             })
